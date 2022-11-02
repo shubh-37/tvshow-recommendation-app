fe70b38633296c1b370e2b2e78118374f62f22ad
Shubh Arya <shubh.arya37@gmail.com>
2022-11-03 00:19:57 +0530

    created view (#1)
    * Update 2 files
    
    * Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,72 @@
+import React from "react";
 import "./styles.css";
 
 export default function App() {
+  const tvshowDict = {
+    Sitcom: {
+      Friends: {
+        Rating: "4/5",
+        Description: "",
+      },
+      B99: {
+        Rating: "4.5/5",
+        Description: "",
+      },
+      "Modern Family": {
+        Rating: "3.5/5",
+        Description: "",
+      },
+    },
+    Action: {
+      "The Witcher": {
+        Rating: "4.5/5",
+        Description: "",
+      },
+      Bodyguard: {
+        Rating: "5/5",
+        Description: "",
+      },
+      "Jack Ryan": {
+        Rating: "5/5",
+        Description: "",
+      },
+    },
+    Thriller: {
+      "Money Heist": {
+        Rating: "4/5",
+        Description: "",
+      },
+      Mirzapur: {
+        Rating: "4.5/5",
+        Description: "",
+      },
+      "Stranger Things": {
+        Rating: "4/5",
+        Description: "",
+      },
+    },
+  };
+  const tvshowArr = Object.keys(tvshowDict);
+  function clickHandler() {
+    console.log("clicked");
+  }
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Favorite TV Show</h1>
+      <h3>
+        Select a genre to know about my favorite TV Shows of each genre
+        respectively.
+      </h3>
+      <div className="genre-btn">
+        <button onClick={clickHandler} name="sitcom">
+          Sitcom
+        </button>
+        <button>Action</button>
+        <button>Thriller</button>
+      </div>
+      <div>
+        <div>{}</div>
+      </div>
     </div>
   );
 }
