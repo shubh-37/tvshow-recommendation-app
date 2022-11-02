b56dec45e52fd910e19cdc04ed0e10ef0be21f89
Shubh Arya <shubh.arya37@gmail.com>
2022-11-02 19:18:28 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,53 +2,64 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  const tvshowDict = {
-    Sitcom: {
-      Friends: {
-        Rating: "4/5",
-        Description: "",
-      },
-      B99: {
-        Rating: "4.5/5",
-        Description: "",
-      },
-      "Modern Family": {
-        Rating: "3.5/5",
-        Description: "",
-      },
+  const tvShows = [
+    { name: "Friends", rating: "4/5", description: "", genre: "Sitcom" },
+    {
+      name: "Brooklyn 99",
+      rating: "4.5/5",
+      description: "",
+      genre: "Sitcom",
     },
-    Action: {
-      "The Witcher": {
-        Rating: "4.5/5",
-        Description: "",
-      },
-      Bodyguard: {
-        Rating: "5/5",
-        Description: "",
-      },
-      "Jack Ryan": {
-        Rating: "5/5",
-        Description: "",
-      },
+    {
+      name: "Modern Family",
+      rating: "3.5/5",
+      description: "",
+      genre: "Sitcom",
     },
-    Thriller: {
-      "Money Heist": {
-        Rating: "4/5",
-        Description: "",
-      },
-      Mirzapur: {
-        Rating: "4.5/5",
-        Description: "",
-      },
-      "Stranger Things": {
-        Rating: "4/5",
-        Description: "",
-      },
+    {
+      name: "The Witcher",
+      rating: "4.5/5",
+      description: "",
+      genre: "Action",
     },
-  };
-  const tvshowArr = Object.keys(tvshowDict);
-  function clickHandler() {
+    {
+      name: "Bodyguard",
+      rating: "5/5",
+      description: "",
+      genre: "Action",
+    },
+    {
+      name: "Jack Ryan",
+      rating: "5/5",
+      description: "",
+      genre: "Action",
+    },
+    {
+      name: "Money Heist",
+      rating: "4/5",
+      description: "",
+      genre: "Thriller",
+    },
+    {
+      name: "Mirzapur",
+      rating: "4.5/5",
+      description: "",
+      genre: "Thriller",
+    },
+    {
+      name: "Stranger Things",
+      Rating: "4/5",
+      Description: "",
+      genre: "Thriller",
+    },
+  ];
+  let filteredArr = [];
+  function clickHandler(event) {
     console.log("clicked");
+    const name = event.target.getAttribute("name");
+    console.log({ name });
+    filteredArr = tvShows.filter((obj) => obj.genre === name);
+    console.log({ filteredArr });
   }
   return (
     <div className="App">
@@ -58,14 +69,22 @@ export default function App() {
         respectively.
       </h3>
       <div className="genre-btn">
-        <button onClick={clickHandler} name="sitcom">
+        <button onClick={clickHandler} name="Sitcom">
           Sitcom
         </button>
-        <button>Action</button>
-        <button>Thriller</button>
+        <button onClick={clickHandler} name="Action">
+          Action
+        </button>
+        <button onClick={clickHandler} name="Thriller">
+          Thriller
+        </button>
       </div>
       <div>
-        <div>{}</div>
+        <div>
+          {filteredArr.map((obj) => (
+            <span>{obj.name}</span>
+          ))}
+        </div>
       </div>
     </div>
   );
